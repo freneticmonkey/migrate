8c1382515bfd3946f899f19dc7fb95f8f0ccfae4
freneticmonkey <scottporter@neuroticstudios.com>
2016-05-27 08:16:23 +1000

    Implemented diff command

--- migrate.go
+++ migrate.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/freneticmonkey/migrate/migrate/cmd"
 	"github.com/freneticmonkey/migrate/migrate/config"
+	"github.com/freneticmonkey/migrate/migrate/management"
 	"github.com/freneticmonkey/migrate/migrate/util"
 	"github.com/freneticmonkey/migrate/migrate/yaml"
 )
@@ -47,12 +48,31 @@ func main() {
 	// Configure the app
 
 	app.Flags = cmd.GetGlobalFlags()
+
 	app.Commands = []cli.Command{
 		cmd.GetSetupCommand(),
-		cmd.GetDiffCommand(),
+		cmd.GetDiffCommand(&conf),
 		cmd.GetValidateCommand(),
 		cmd.GetCreateCommand(),
 		cmd.GetExecCommand(),
+	}
+	app.Before = func(ctx *cli.Context) error {
+
+		configURL := ctx.String("config-url")
+		err := yaml.ReadFile(configURL, &conf)
+		if !util.ErrorCheckf(err, "Configuration read failed for: %s", configURL) {
+			util.LogInfo("Configuration Read Success: " + configURL)
+
+			// Initialise any utility configuration
+			util.Config(conf)
+
+			// Configure access to the management DB
+			management.Setup(conf)
+
+		} else {
+			return cli.NewExitError("Unable to read configuration: "+configURL, 1)
+		}
+		return nil
 	}
 
 	app.Run(os.Args)
--- migrate/cmd/diff.go
+++ migrate/cmd/diff.go
@@ -1,13 +1,49 @@
 package cmd
 
-import "github.com/urfave/cli"
+import (
+	"github.com/freneticmonkey/migrate/migrate/config"
+	"github.com/freneticmonkey/migrate/migrate/git"
+	"github.com/freneticmonkey/migrate/migrate/mysql"
+	"github.com/freneticmonkey/migrate/migrate/table"
+	"github.com/freneticmonkey/migrate/migrate/util"
+	"github.com/freneticmonkey/migrate/migrate/yaml"
+	"github.com/urfave/cli"
+)
 
 // GetDiffCommand Configure the validate command
-func GetDiffCommand() (setup cli.Command) {
+func GetDiffCommand(conf *config.Config) (setup cli.Command) {
 	setup = cli.Command{
 		Name:  "diff",
 		Usage: "Diff the MySQL target database and the YAML schema.",
-		Action: func(c *cli.Context) error {
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "project",
+				Value: "",
+				Usage: "The target project",
+			},
+			cli.StringFlag{
+				Name:  "version",
+				Value: "",
+				Usage: "The target git version",
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			if ctx.IsSet("project") && ctx.IsSet("version") {
+				git.Clone(conf.Project)
+			}
+
+			// Read the YAML files cloned from the repo
+			err := yaml.ReadTables(conf.Options.WorkingPath)
+			if util.ErrorCheck(err) {
+				return cli.NewExitError("Diff failed. Unable to read YAML Tables", 1)
+			}
+			// Read the MySQL tables from the target database
+			err = mysql.ReadTables(conf.Project)
+			if util.ErrorCheck(err) {
+				return cli.NewExitError("Diff failed. Unable to read MySQL Tables", 1)
+			}
+			forwardDiff := table.DiffTables(yaml.Schema, mysql.Schema)
+			mysql.GenerateAlters(forwardDiff)
 
 			return cli.NewExitError("Diff completed successfully.", 0)
 		},
