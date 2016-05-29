91f85e63bad95a11c4b00458842f3c90a49bf2c7
freneticmonkey <scottporter@neuroticstudios.com>
2016-05-29 10:38:17 +1000

    Implemented Migration Execute command - untested

--- migrate/cmd/exec.go
+++ migrate/cmd/exec.go
@@ -1,6 +1,12 @@
 package cmd
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+
+	"github.com/freneticmonkey/migrate/migrate/exec"
+	"github.com/freneticmonkey/migrate/migrate/util"
+	"github.com/urfave/cli"
+)
 
 // GetExecCommand Execute the migration indicated by the id flag
 func GetExecCommand() (setup cli.Command) {
@@ -10,21 +16,55 @@ func GetExecCommand() (setup cli.Command) {
 		Flags: []cli.Flag{
 			cli.IntFlag{
 				Name:  "id",
-				Value: -1,
-				Usage: "The id of the migration to execute",
+				Value: 0,
+				Usage: "The id of the migration to be run",
 			},
 			cli.BoolFlag{
 				Name:  "dryrun",
-				Usage: "Execute a dryrun of the migration",
+				Usage: "Peform a dry run of the migration",
 			},
 			cli.BoolFlag{
 				Name:  "rollback",
-				Usage: "Allow for a rollback migration to be executed.",
+				Usage: "Allows for a rollback to be created",
+			},
+			cli.BoolFlag{
+				Name:  "pto-disabled",
+				Usage: "Execute the migration without using pt-online-schema-change.",
+			},
+			cli.BoolFlag{
+				Name:  "allow-destructive",
+				Usage: "Explictly allow rename and delete migration actions",
 			},
 		},
-		Action: func(c *cli.Context) error {
+		Action: func(ctx *cli.Context) error {
+			var mid int64
 
-			return cli.NewExitError("Migration Exec completed successfully.", 0)
+			if ctx.IsSet("id") && ctx.Int("id") > 0 {
+				mid = int64(ctx.Int("id"))
+			} else {
+				return cli.NewExitError("Exec failed. No Migration Id set", 1)
+			}
+
+			dryrun := ctx.Bool("dryrun")
+			rollback := ctx.Bool("rollback")
+			PTODisabled := ctx.Bool("pto-disabled")
+			allowDestructive := ctx.Bool("allow-destructive")
+
+			err := exec.Exec(exec.ExecOptions{
+				MID:              mid,
+				Dryrun:           dryrun,
+				Rollback:         rollback,
+				PTODisabled:      PTODisabled,
+				AllowDestructive: allowDestructive,
+			})
+
+			if util.ErrorCheck(err) {
+				errmsg := fmt.Sprintf("Execute failed. Unable to execute new Migration with ID: [%d].", mid)
+				return cli.NewExitError(errmsg, 1)
+			}
+
+			success := fmt.Sprintf("Migration successfully with ID: %d", mid)
+			return cli.NewExitError(success, 0)
 		},
 	}
 	return setup
