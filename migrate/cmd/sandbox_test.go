60cca476b458add82f5d6e62d07f17f8c41177e0
freneticmonkey <scottporter@neuroticstudios.com>
2016-07-31 01:42:50 +1000

    Making progress on the sandbox database refresh unit test, but still more work to go.

--- migrate/cmd/sandbox_test.go
+++ migrate/cmd/sandbox_test.go
@@ -1,17 +1,50 @@
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"regexp"
 	"testing"
 
+	"github.com/go-gorp/gorp"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
+
 	"github.com/freneticmonkey/migrate/migrate/config"
+	"github.com/freneticmonkey/migrate/migrate/management"
 	"github.com/freneticmonkey/migrate/migrate/util"
 )
 
-func TestConfigReadURL(t *testing.T) {
+var mockDb *sql.DB
+var mock sqlmock.Sqlmock
+
+// Configure Gorp with Mock DB
+func dbSetup() (gdb *gorp.DbMap, err error) {
+
+	mockDb, mock, err = sqlmock.New()
+
+	if err != nil {
+		return nil, err
+	}
+
+	gdb = &gorp.DbMap{
+		Db: mockDb,
+		Dialect: gorp.MySQLDialect{
+			Engine:   "InnoDB",
+			Encoding: "UTF8",
+		},
+	}
+
+	return gdb, err
+}
+
+func dbTearDown() {
+	mockDb.Close()
+}
+
+func DisableTestConfigReadURL(t *testing.T) {
 
 	// TODO: Provide config
 	var remoteConfig = `
@@ -81,16 +114,84 @@ func TestConfigReadURL(t *testing.T) {
 }
 
 func TestRefreshDatabase(t *testing.T) {
-	// Configure management
+	util.SetVerbose(true)
+	// Test Configuration
 
-	// Setup Configuration somehow
+	testConfig := config.Config{
+		Project: config.Project{
+			Name: "animals",
+			DB: config.DB{
+				Username:    "root",
+				Password:    "test",
+				Ip:          "127.0.0.1",
+				Port:        3500,
+				Database:    "test",
+				Environment: "SANDBOX",
+			},
+		},
+	}
+
+	// Configure the management gorp DB
+
+	mdb, err := dbSetup()
+
+	if err != nil {
+		t.Errorf("Test Refresh Database: Setup Management DB Failed with Error: %v", err)
+	} else {
+		management.SetManagementDB(mdb)
+	}
+
+	// TODO: Add mock db expect table 'database' accesses here.
+
+	mock.ExpectQuery("SHOW TABLES IN management").
+		WillReturnRows(sqlmock.NewRows([]string{
+			"tables",
+		}).
+			AddRow("metadata").
+			AddRow("migration").
+			AddRow("migration_steps").
+			AddRow("target_database"))
+
+	query := fmt.Sprintf("SELECT * FROM target_database WHERE project=\"%s\" AND name=\"%s\" AND env=\"%s\"",
+		testConfig.Project.Name,
+		testConfig.Project.DB.Database,
+		testConfig.Project.DB.Environment,
+	)
+	query = regexp.QuoteMeta(query)
+
+	mock.ExpectQuery(query).
+		WillReturnRows(sqlmock.NewRows([]string{
+			"dbid",
+			"project",
+			"name",
+			"env",
+		}).AddRow(
+			1,
+			testConfig.Project.Name,
+			testConfig.Project.DB.Database,
+			testConfig.Project.DB.Environment,
+		))
+
+	// Configure management
+	setConfig(testConfig)
 
 	// Reset/recreate management database?
 
+	// TODO: Add mock db expect database recreation statements here.
+
 	// Setup expected database operations
 
+	query = "select count(*) from migration"
+	query = regexp.QuoteMeta(query)
+	mock.ExpectQuery(query).
+		WillReturnRows(sqlmock.NewRows([]string{
+			"count",
+		}).AddRow(
+			0,
+		))
+
 	// Execute the migration
-	err := sandboxProcessFlags(true, false, false, false)
+	err = sandboxProcessFlags(testConfig, true, false, false, true)
 
 	if err != nil {
 		t.Errorf("Sandbox Refresh FAILED with Error: %v", err)
