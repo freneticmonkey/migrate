14295787ef9478c65b3d93de958d060642998ece
freneticmonkey <scottporter@neuroticstudios.com>
2016-08-07 08:29:12 +1000

    Added unit test for Management DB Setup

--- go/cmd/setup_test.go
+++ go/cmd/setup_test.go
@@ -4,15 +4,14 @@ import (
 	"testing"
 
 	"github.com/freneticmonkey/migrate/go/management"
+	"github.com/freneticmonkey/migrate/go/metadata"
+	"github.com/freneticmonkey/migrate/go/mysql"
 	"github.com/freneticmonkey/migrate/go/test"
-	"github.com/freneticmonkey/migrate/go/util"
 )
 
 func TestManagementSetup(t *testing.T) {
 	var mgmtDB test.ManagementDB
 	var err error
-
-	util.SetVerbose(true)
 
 	testName := "TestSetupManagementDB"
 
@@ -61,8 +60,6 @@ func TestManagementSetup(t *testing.T) {
 func TestBuildSchema(t *testing.T) {
 	var mgmtDB test.ManagementDB
 	var err error
-
-	util.SetVerbose(true)
 
 	testName := "TestSetupManagementDB"
 
@@ -122,5 +119,78 @@ func TestBuildSchema(t *testing.T) {
 }
 
 func TestSetupExistingDB(t *testing.T) {
+	var mgmtDB test.ManagementDB
+	var projectDB test.ProjectDB
+	var err error
 
+	testName := "TestSetupManagementDB"
+
+	// Configuration
+	testConfig := GetTestConfig()
+
+	dogsTbl := GetTableDogs()
+
+	// Setup the mock Managment DB
+	mgmtDB, err = test.CreateManagementDB(testName, t)
+
+	// Setup the mock Project DB
+
+	projectDB, err = test.CreateProjectDB(testName, t)
+
+	mysql.Setup(testConfig)
+
+	// Configure metadata
+	metadata.Setup(mgmtDB.Db, 1)
+
+	// Connect to Project DB
+	mysql.SetProjectDB(projectDB.Db.Db)
+
+	// SHOW TABLES Query
+	projectDB.ShowTables([]test.DBRow{{dogsTbl.Name}}, false)
+
+	// SHOW CREATE TABLE Query
+	projectDB.ShowCreateTable(dogsTbl.Name, GetMySQLCreateTableDogs())
+
+	// Load Table Metadata - Expect empty because this is a new database
+	mgmtDB.MetadataSelectName(
+		"dogs",
+		test.DBRow{},
+		// test.DBRow{1, 1, "tbl1", "", "Table", "dogs", 1},
+		true,
+	)
+
+	// mgmtDB.MetadataLoadAllTableMetadata("tbl1",
+	// 	1,
+	// 	[]test.DBRow{
+	// 		test.DBRow{1, 1, "tbl1", "", "Table", "dogs", 1},
+	// 		test.DBRow{2, 1, "col1", "tbl1", "Column", "id", 1},
+	// 		test.DBRow{3, 1, "pi", "tbl1", "PrimaryKey", "PrimaryKey", 1},
+	// 	},
+	// 	false,
+	// )
+
+	// metadata insert
+	mgmtDB.MetadataInsert(
+		test.DBRow{1, "tbl1", "", "Table", "dogs", 1},
+		1,
+		1,
+	)
+
+	// metadata insert
+	mgmtDB.MetadataInsert(
+		test.DBRow{1, "col1", "tbl1", "Column", "id", 1},
+		2,
+		1,
+	)
+
+	// metadata insert
+	mgmtDB.MetadataInsert(
+		test.DBRow{1, "pi", "tbl1", "PrimaryKey", "PrimaryKey", 1},
+		3,
+		1,
+	)
+
+	if err != nil {
+		t.Errorf("%s FAILED with err: %v", testName, err)
+	}
 }
