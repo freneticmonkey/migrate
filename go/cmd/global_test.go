1d5ecf0c06cb454288e829923f5abb70dc8d0e42
freneticmonkey <scottporter@neuroticstudios.com>
2016-08-09 22:58:03 +1000

    Added Config file reading unit test

--- go/cmd/global_test.go
+++ go/cmd/global_test.go
@@ -9,8 +9,10 @@ import (
 	"testing"
 
 	"github.com/freneticmonkey/migrate/go/config"
+	"github.com/freneticmonkey/migrate/go/management"
 	"github.com/freneticmonkey/migrate/go/metadata"
 	"github.com/freneticmonkey/migrate/go/table"
+	"github.com/freneticmonkey/migrate/go/test"
 	"github.com/freneticmonkey/migrate/go/util"
 )
 
@@ -170,6 +172,118 @@ func GetTableAddressDogs() table.Table {
 			Name:       "dogs",
 			Type:       "Table",
 		},
+	}
+}
+
+func TestConfigReadFile(t *testing.T) {
+	var mgmtDB test.ManagementDB
+	testName := "TestConfigReadFile"
+
+	// TODO: Provide config
+	configFilename := "config.yml"
+	var configContents = `
+    options:
+        management:
+            db:
+                username: root
+                password: test
+                ip:       127.0.0.1
+                port:     3400
+                database: management
+
+    # Project Definition
+    project:
+        # Project name - used to identify the project by the cli flags
+        # and configure the table's namespace
+        name: "animals"
+        db:
+            username:    root
+            password:    test
+            ip:          127.0.0.1
+            port:        3500
+            database:    test
+            environment: UNITTEST
+    `
+	expectedConfig := config.Config{
+		Options: config.Options{
+			Management: config.Management{
+				DB: config.DB{
+					Username:    "root",
+					Password:    "test",
+					Ip:          "127.0.0.1",
+					Port:        3400,
+					Database:    "management",
+					Environment: "",
+				},
+			},
+		},
+		Project: config.Project{
+			Name: "animals",
+			DB: config.DB{
+				Username:    "root",
+				Password:    "test",
+				Ip:          "127.0.0.1",
+				Port:        3500,
+				Database:    "test",
+				Environment: "UNITTEST",
+			},
+		},
+	}
+
+	// Set Testing FileSystem
+	util.SetConfigTesting()
+	util.Config(expectedConfig)
+
+	// Write a test configuration YAML file
+	err := util.WriteFile(configFilename, []byte(configContents), 0644)
+
+	if err != nil {
+		t.Errorf("Config Read File: Write test config FAILED with Error: %v", err)
+		return
+	}
+
+	// manually setting the default global config filename
+	configFile = configFilename
+
+	// Check for mananagement tables
+
+	// Setup the mock Managment DB
+	mgmtDB, err = test.CreateManagementDB(testName, t)
+
+	// If we have the tables
+	mgmtDB.ShowTables(
+		[]test.DBRow{
+			{"metadata"},
+			{"migration"},
+			{"migration_steps"},
+			{"target_database"},
+		},
+		false,
+	)
+
+	//  Get Database from Project table - Add an entry for the SANDBOX database
+	mgmtDB.DatabaseGet(
+		expectedConfig.Project.Name,
+		expectedConfig.Project.DB.Database,
+		expectedConfig.Project.DB.Environment,
+		test.DBRow{1, "UnitTestProject", "project", "SANDBOX"},
+		false,
+	)
+
+	// Set the management DB
+	management.SetManagementDB(mgmtDB.Db)
+
+	fileConfig, err := configureManagement()
+
+	if err != nil {
+		t.Errorf("Config Read File FAILED with Error: %v", err)
+		return
+	}
+
+	if !reflect.DeepEqual(expectedConfig, fileConfig) {
+		t.Error("Config Read File FAILED. Returned config does not match.")
+		util.LogWarn("Config Read File FAILED. Returned config does not match.")
+		util.DebugDumpDiff(expectedConfig, fileConfig)
 	}
 }
 
