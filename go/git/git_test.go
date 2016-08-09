02673c6343e0675c8500bd549aeec5d44f12f380
freneticmonkey <scottporter@neuroticstudios.com>
2016-08-09 23:57:38 +1000

    Created sub unit test for git package

---
+++ go/git/git_test.go
@@ -0,0 +1,19 @@
+package git
+
+import "testing"
+
+func TestGetVersionTime(t *testing.T) {
+
+}
+
+func TestGetVersion(t *testing.T) {
+
+}
+
+func TestGetVersionDetails(t *testing.T) {
+
+}
+
+func TestClone(t *testing.T) {
+
+}
