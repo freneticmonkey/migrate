4513e8af34a47c0857c421a4c4355cda7b4a7c8e
freneticmonkey <scottporter@neuroticstudios.com>
2016-08-17 18:00:07 +1000

    Added util function for generating MySQL Datetime strings from go time structs

--- go/mysql/const.go
+++ go/mysql/const.go
@@ -7,5 +7,10 @@ var TimeFormat = `2006-01-02 15:04:05`
 
 // GetTimeNow Return the current time as a string in MySQL time format
 func GetTimeNow() string {
-	return time.Now().UTC().Format(TimeFormat)
+	return FormatTime(time.Now())
+}
+
+// FormatTime Return the current time as a string in MySQL time format
+func FormatTime(tm time.Time) string {
+	return tm.UTC().Format(TimeFormat)
 }
