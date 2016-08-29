2a9bc9c11dc22864470f9d85a00dbfac649f0097
freneticmonkey <scottporter@neuroticstudios.com>
2016-08-30 01:05:10 +1000

    Added addition Template Configuration validation

--- go/sandbox/generate.go
+++ go/sandbox/generate.go
@@ -20,10 +20,16 @@ func Generate(conf config.Config, tableName string) (err error) {
 	var f *os.File
 	var exists bool
 
-	templateFile := util.WorkingSubDir(conf.Options.Template.File)
+	tmpOptions := conf.Options.Template
+
+	if tmpOptions.Path == "" || tmpOptions.File == "" || tmpOptions.Ext == "" {
+		return fmt.Errorf("Generate Table: Badly configured template options")
+	}
+
+	templateFile := util.WorkingSubDir(tmpOptions.File)
 
 	// Generation path
-	genPath := util.WorkingSubDir(conf.Options.Template.Path)
+	genPath := util.WorkingSubDir(tmpOptions.Path)
 
 	// Ensure that the path exists
 	exists, err = util.DirExists(genPath)
@@ -75,7 +81,7 @@ func Generate(conf config.Config, tableName string) (err error) {
 	// Generate each table
 	for _, tbl := range yaml.Schema {
 		// Build a filename and path for the table file
-		tblFilename := fmt.Sprintf("%s.%s", strings.ToLower(tbl.Name), conf.Options.Template.Ext)
+		tblFilename := fmt.Sprintf("%s.%s", strings.ToLower(tbl.Name), tmpOptions.Ext)
 
 		// Create a file
 		f, err = os.Create(filepath.Join(genPath, tblFilename))
@@ -93,5 +99,66 @@ func Generate(conf config.Config, tableName string) (err error) {
 	}
 
 	return err
+}
 
+// GenerateTable Serialise a table using the configured template
+func GenerateTable(conf config.Config, tbl table.Table) (err error) {
+	var data []byte
+	var tmpl *template.Template
+	var f *os.File
+	var exists bool
+
+	tmpOptions := conf.Options.Template
+
+	if tmpOptions.Path == "" || tmpOptions.File == "" || tmpOptions.Ext == "" {
+		return fmt.Errorf("Generate Table: Badly configured template options")
+	}
+
+	templateFile := util.WorkingSubDir(tmpOptions.File)
+
+	// Generation path
+	genPath := util.WorkingSubDir(tmpOptions.Path)
+
+	// Ensure that the path exists
+	exists, err = util.DirExists(genPath)
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		err = util.Mkdir(genPath, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	data, err = util.ReadFile(templateFile)
+	if err != nil {
+		return err
+	}
+	tblTmpl := string(data)
+
+	tmpl, err = template.New("t").Parse(tblTmpl)
+	if err != nil {
+		return err
+	}
+
+	// Build a filename and path for the table file
+	tblFilename := fmt.Sprintf("%s.%s", strings.ToLower(tbl.Name), tmpOptions.Ext)
+
+	// Create a file
+	f, err = os.Create(filepath.Join(genPath, tblFilename))
+	if err != nil {
+		return err
+	}
+
+	// Generate the contents from the template
+	err = tmpl.Execute(f, tbl)
+	if err != nil {
+		f.Close()
+		return err
+	}
+	f.Close()
+
+	return err
 }
